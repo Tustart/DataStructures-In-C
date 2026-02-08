f531fe47e4e2455766b539c31aa3b6ba6045134d
Tustart <2230131060@qq.com>
2026-02-08 17:55:43 +0800

    feat:完成了一些公共头文件common.h的编写,简写了一些大纲

---
+++ common/common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+//引入标准库
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+//定义常用状态码
+#define OK          1
+#define ERROR       0
+#define TURE        1
+#define FALSE       0
+#define OVERFLOW   -1
+//函数返回值类型
+typedef int Status;
+#endif
